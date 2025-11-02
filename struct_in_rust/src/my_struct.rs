2b100967707cf0a15cb294e11fd1b79d22c71a23
Jethro Lopwus <lopwusjethro92@gmail.com>
2025-11-02 09:59:49 +0100

    update: use Cell time form rust standard module to modify a specific field in a struct

--- struct_in_rust/src/my_struct.rs
+++ struct_in_rust/src/my_struct.rs
@@ -1,6 +1,7 @@
- struct Person {
-
-     fist_name: String,
+ use std::cell::Cell;
+ 
+ struct Person<'p> {
+     first_name: Cell<&'p str>,
      last_name: String,
      birth_year: u16,
      birth_month: u8,
@@ -36,22 +37,29 @@ struct Vehicle {
 struct VehicleTupple(String, String, u16, VehicleColors, bool);
 
 
- fn new_person() -> Person {
+ fn new_person() -> Person <'static> {
+    // mut keyword added to make the struct mutable
    let p1 = Person{ 
-       fist_name: "Jethro".to_string(),
+    // using Cell here from the standard library //
+       first_name: Cell::from ("Jethro"),
        last_name: String::from("Brown"),
        birth_year: 1995,
        birth_month: 8,
        is_married: false,
+
       
    };
+// mutating struct fields
+    p1.first_name.set( "Micheal");
    return p1;
 
 }
 
 pub fn create_person() {
    let my_person = new_person();
-    println!("First Name: {0}, Last Name: {1}, Birth Year: {2}, Birth Month: {3}, is Married: {4} ", my_person.fist_name, my_person.last_name, my_person.birth_year, my_person.birth_month, my_person.is_married);
+    println!("First Name: {0}, Last Name: {1}, Birth Year: {2}, Birth Month: {3}, is Married: {4} ", my_person.first_name.get(), my_person.last_name, my_person.birth_year, my_person.birth_month, my_person.is_married);
+
+    
 }
 
 
@@ -79,5 +87,10 @@ fn new_vehicle_tupple() -> VehicleTupple {
 }   
 
 pub fn create_new_vehicletupple () {
+    // to acces each field 
+    let my_vehicle_tupple = new_vehicle_tupple();
    println!("{:?}", new_vehicle_tupple());
+//    to access individual field index here //
+
+println!("Manufacturer: {0}, Model: {1}, Year: {2}, Color: {3:?}, Is Electric: {4}", my_vehicle_tupple.0, my_vehicle_tupple.1, my_vehicle_tupple.2, my_vehicle_tupple.3, my_vehicle_tupple.4 );
 }
